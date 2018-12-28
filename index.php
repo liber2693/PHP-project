3b4f251f388dbafccd65a4562d8439dc4e8e1b89
liber2693 <jrm@dementecreativo.com>
2018-12-28 16:06:10 -0400

    28-12-2018 0406

--- index.php
+++ index.php
@@ -711,54 +711,7 @@
   <a class="scrolltop" href="#"><span class="fa fa-angle-up"></span></a>
 
 	<!-- Modal -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-sm" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Sistema Administrativo</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<form method="post" id="login">
-					<div class="modal-body">
-						<div class="alert alert-danger oculto" id="error" role="alert">
-							<strong>Ingresar Usuario</strong>
-						</div>
-						<div class="alert alert-warning oculto" id="error2" role="alert">
-						 	<strong>Verifique el usuario</strong>
-						</div>
-						<div class="form-group">
-							<label for="recipient-name" class="col-form-label" placeholder="usuario">
-								Usuario:
-							</label>
-							<input type="text" class="form-control" id="user">
-						</div>
-						<div class="alert alert-danger oculto" id="error1" role="alert">
-							<strong>Ingresar Contraseña</strong>
-						</div>
-						<div class="alert alert-warning oculto" id="error3" role="alert">
-						 	<strong>Verifique contraseña</strong>
-						</div>
-						<div class="form-group">
-							<label for="recipient-name" class="col-form-label" placeholder="contraseña">
-								Contraseña:
-							</label>
-							<input type="password" class="form-control" id="password">
-						</div>
-					</div>
-					<div class="modal-footer">
-						<button type="submit" class="btn btn-primary" id="enviar">
-								Login IN
-						</button>
-						<button type="button" class="btn btn-danger" data-dismiss="modal">
-							Cerrar
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+	<?php include 'views/login.php'; ?>
 	<!-- fin Modal -->
 
 
--- views/login.php
+++ views/login.php
@@ -1,73 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <link rel="shortcut icon" href="../images/unnamed.png">
-  <meta charset="utf-8">
-  <!-- <link rel="shortcut icon" href="theme/img/icono1.png"> -->
-  <title>Fortaleza y Constancia</title>
-  <!-- Bootstrap CSS -->
-  <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-  <!-- bootstrap theme -->
-  <link href="../assets/css/bootstrap-theme.css" rel="stylesheet">
-  <!--external css-->
-  <!-- font icon -->
-  <link href="../assets/css/elegant-icons-style.css" rel="stylesheet" />
-  <link href="../assets/css/font-awesome.css" rel="stylesheet" />
-  <!-- Custom styles -->
-  <link href="../assets/css/style.css" rel="stylesheet">
-  <link href="../css/estilos.css" rel="stylesheet" />
-</head>
-<body background="../images/1.jpg">
-  <br><br>
-  <div>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="../index.php"><i class="fa fa-rotate-left"><b>&nbsp;&nbsp; Volver</b></i></a>
-  </div>
-  <div class="container">
-    <!-- <center><img src="#" width="160" height="90"></center><br><br> --><br><br><br><br><br>
-<form class="login-form" name="login" id="login">
-<!--  <form class="login-form" method="post" action="../controllers/sesionController.php"> -->
-  <div class="row">
-    <div class="col-sm-4 col-sm-offset-4">
-      <label><b>USUARIO</b></label>
-      <div class="alert danger ocultar" id="error2"><strong>Usuario Incorrecto</strong></div>
-      <div class="alert success ocultar" id="error"><strong>Por favor, logueate</strong></div>
-      <div class="input-group">
-        <span class="input-group-addon"><i class="icon_profile"></i></span>
-        <input type="text" name="usuario" id="usuario" class="form-control" placeholder="Nombre de Usuario" autofocus>
-      </div>
-    </div>
-  </div>
-<br>
-  <div class="row">
-    <div class="col-sm-4 col-sm-offset-4">
-      <label><b>CONTRASEÑA</b></label>
-      <div class="alert success ocultar" id="error1"><strong>Por favor, Introduzca su Contraseña</strong></div>
-      <div class="alert danger ocultar" id="error3"><strong>Contraseña Incorrecta</strong></div>
-      <div class="input-group">
-        <span class="input-group-addon"><i class="icon_key_alt"></i></span>
-        <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña" autocomplete="off">
-      </div>
-    </div>
-  </div>
-<br><br>
-<div class="row">
-  <div class="col-sm-6 col-sm-offset-4">
-    <div class="col-sm-4">
-      <!--<button class="btn btn-success btn-md btn-block" type="submit" id="enviar"><b>Entrar</b></button>-->
-      <button class="btn btn-success btn-md btn-block" type="submit"><b>Entrar</b></button>
-    </div>
-    <div class="col-sm-4">
-      <button class="btn btn-danger btn-md btn-block" type="reset"><b>Limpiar</b></button>
-  </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Sistema Administrativo</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form method="post" id="login">
+				<div class="modal-body">
+					<div class="alert alert-danger oculto" id="error" role="alert">
+						<strong>Ingresar Usuario</strong>
+					</div>
+					<div class="alert alert-warning oculto" id="error2" role="alert">
+					 	<strong>Verifique el usuario</strong>
+					</div>
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label" placeholder="usuario">
+							Usuario:
+						</label>
+						<input type="text" class="form-control" id="user">
+					</div>
+					<div class="alert alert-danger oculto" id="error1" role="alert">
+						<strong>Ingresar Contraseña</strong>
+					</div>
+					<div class="alert alert-warning oculto" id="error3" role="alert">
+					 	<strong>Verifique contraseña</strong>
+					</div>
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label" placeholder="contraseña">
+							Contraseña:
+						</label>
+						<input type="password" class="form-control" id="password">
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary" id="enviar">
+							Login IN
+					</button>
+					<button type="button" class="btn btn-danger" data-dismiss="modal">
+						Cerrar
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
-</div>
-</form>
-
-  </div>
-</body>
-  <!-- javascritp -->
-  <script src="../assets/js/jquery-2.1.1.min.js"></script>
-  <script src="../js/usuario.js"></script>
-</html>
